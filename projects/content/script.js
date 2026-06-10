7c43a03d20fd20f34bcfa7d94b4b13b1d9a41c6c
lizbeth-dev <kagendoliz2006@gmail.com>
2026-06-10 14:02:26 +0300

    Add files via upload

---
+++ projects/content/script.js
@@ -0,0 +1,26 @@
+let fName ="Lizbeth"
+let lName ="Kagendo"
+console.log(fName + " " + lName)
+var marks=450
+console.log(450)
+var fTermfees=20000
+console.log(20000)
+var sTermfees=30000
+console.log(30000)
+let gpa=7.0
+console.log(gpa)
+let pass =false
+console.log(pass)
+let mainName = document.getElementById("fN")
+mainName.innerHTML=fName
+let secName = document.getElementById("sN")
+secName.innerHTML=lName
+let pMarks = document.getElementById("marks")
+pMarks.innerHTML =marks
+let tGpa = document.getElementById("gpa")
+tGpa.innerHTML =gpa
+let fTermfees1 = document.getElementById("fT")
+fTermfees1.innerHTML =fTermfees
+let sTermfees4 =document.getElementById("sT")
+sTermfees4.innerHTML =sTermfees
+
