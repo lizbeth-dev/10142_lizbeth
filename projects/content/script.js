9891e5f9fa41ea47d53c430c86c2f282adc3180a
lizbeth-dev <kagendoliz2006@gmail.com>
2026-06-11 13:13:01 +0300

    Add files via upload

--- projects/content/script.js
+++ projects/content/script.js
@@ -1,4 +1,4 @@
-let fName ="Lizbeth"
+/*let fName ="Lizbeth"
 let lName ="Kagendo"
 console.log(fName + " " + lName)
 var marks=450
@@ -7,6 +7,9 @@ var fTermfees=20000
 console.log(20000)
 var sTermfees=30000
 console.log(30000)
+var tTermFees=(40000)
+console.log(40000)
+var totalYearlyFees=fTermfees+sTermfees+tTermFees
 let gpa=7.0
 console.log(gpa)
 let pass =false
@@ -23,4 +26,56 @@ let fTermfees1 = document.getElementById("fT")
 fTermfees1.innerHTML =fTermfees
 let sTermfees4 =document.getElementById("sT")
 sTermfees4.innerHTML =sTermfees
+let tTermFees3 =document.getElementById("tT")
+tTermFees3.innerHTML =tTermFees
+let totalFees =document.getElementById("total")
+totalFees.innerHTML =totalYearlyFees
+
+
+let totalFees=(12)
+console.log(12)
+
+*/
+
+
+
+let cL=(2000)
+console.log(2000)
+let cK=(4000)
+console.log(4000)
+let cG=(6000)
+console.log(6000)
+
+let tcL=(12)
+console.log(12)
+let tcK=(15)
+console.log(15)
+let tcG=(20)
+console.log(20)
+
+var speedCar1=cL/tcL
+var speedCar2=cK/tcK
+var speedCar3=cG/tcG
+let car1 = document.getElementById("cR")
+car1.innerHTML=cL
+let car2 = document.getElementById("cT")
+car2.innerHTML=cK
+let car3 = document.getElementById("cD")
+car3.innerHTML=cG
+let time1 = document.getElementById("tT")
+time1.innerHTML=tcL
+let time2 = document.getElementById("tA")
+time2.innerHTML=tcK
+let time3 = document.getElementById("tC")
+//time3.innerHTML=tcG
+
+let speed1 = document.getElementById("cars")
+speed1.innerHTML= Math.round(speedCar1)
+let speed2 =document.getElementById("lorry")
+speed2.innerHTML=speedCar2
+let speed3 =document.getElementById("bus")
+speed3.innerHTML=speedCar3
+
+
+
 
