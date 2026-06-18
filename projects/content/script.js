00d8096c6c515b7dd3519323b1a57857dea525aa
lizbeth-dev <kagendoliz2006@gmail.com>
2026-06-18 12:56:20 +0300

    Add files via upload

--- projects/content/script.js
+++ projects/content/script.js
@@ -1,81 +1,37 @@
-/*let fName ="Lizbeth"
-let lName ="Kagendo"
-console.log(fName + " " + lName)
-var marks=450
-console.log(450)
-var fTermfees=20000
-console.log(20000)
-var sTermfees=30000
-console.log(30000)
-var tTermFees=(40000)
-console.log(40000)
-var totalYearlyFees=fTermfees+sTermfees+tTermFees
-let gpa=7.0
-console.log(gpa)
-let pass =false
-console.log(pass)
-let mainName = document.getElementById("fN")
-mainName.innerHTML=fName
-let secName = document.getElementById("sN")
-secName.innerHTML=lName
-let pMarks = document.getElementById("marks")
-pMarks.innerHTML =marks
-let tGpa = document.getElementById("gpa")
-tGpa.innerHTML =gpa
-let fTermfees1 = document.getElementById("fT")
-fTermfees1.innerHTML =fTermfees
-let sTermfees4 =document.getElementById("sT")
-sTermfees4.innerHTML =sTermfees
-let tTermFees3 =document.getElementById("tT")
-tTermFees3.innerHTML =tTermFees
-let totalFees =document.getElementById("total")
-totalFees.innerHTML =totalYearlyFees
+    let displayInterface={
+        title:"ONE PIECE",
+        rating:5.6,
+        seasons :4
+    }
+    //
 
 
-let totalFees=(12)
-console.log(12)
+    displayInterface.rating
+    console.log(displayInterface.title)
+    console.log(displayInterface.rating)
+    console.log(displayInterface.seasons)
+    document.write(displayInterface.title+"<br>")
+    document.write(displayInterface.rating+"<br>")
+    document.write(displayInterface.seasons+"<br>")
 
-*/
+    //ARRAYS
+    let categories=
+    ["anime","lifestyle","anime","drama","horror"]
+    console.log(categories[0])
+    console.log(categories[1])
+    console.log(categories[2])
+    console.log(categories[3])
+    console.log(categories[4])
 
+    let movies=
+    ["onepiece","beauty in black","kiss or kill","dark","blindspot","fast&furious"]
+    console.log(movies[0])
+    console.log(movies[1])
+    console.log(movies[2])
+    console.log(movies[3])
+    console.log(movies[4])
+    console.log(movies[5])
 
-
-let cL=(2000)
-console.log(2000)
-let cK=(4000)
-console.log(4000)
-let cG=(6000)
-console.log(6000)
-
-let tcL=(12)
-console.log(12)
-let tcK=(15)
-console.log(15)
-let tcG=(20)
-console.log(20)
-
-var speedCar1=cL/tcL
-var speedCar2=cK/tcK
-var speedCar3=cG/tcG
-let car1 = document.getElementById("cR")
-car1.innerHTML=cL
-let car2 = document.getElementById("cT")
-car2.innerHTML=cK
-let car3 = document.getElementById("cD")
-car3.innerHTML=cG
-let time1 = document.getElementById("tT")
-time1.innerHTML=tcL
-let time2 = document.getElementById("tA")
-time2.innerHTML=tcK
-let time3 = document.getElementById("tC")
-//time3.innerHTML=tcG
-
-let speed1 = document.getElementById("cars")
-speed1.innerHTML= Math.round(speedCar1)
-let speed2 =document.getElementById("lorry")
-speed2.innerHTML=speedCar2
-let speed3 =document.getElementById("bus")
-speed3.innerHTML=speedCar3
-
-
-
-
+    console.log(displayInterface.rating+" "+categories[1]+" " +displayInterface.title)
+    console.log(displayInterface.seasons+" "+categories[3]+" "+displayInterface.rating)
+    console.log(displayInterface.rating+" "+movies[5]+" "+categories[2])
