be4035c3f9c1ddd4ef4061d70e17b65a365de4de
unknown <KONZA-VDI@GROUP16-043.vdi.konza>
2026-06-24 10:03:05 +0300

    good work

---
+++ classwork/assignment5/index.js
@@ -0,0 +1,27 @@
+
+//warehouse items
+let edibles=
+["bread","crisps<br>"]
+
+let nonedibles=
+["plastic cups<br>"]
+
+let machinery=
+["motocycle engines","electric drills","electricsaw"]
+//display output
+console.log("edibles:")
+console.log(edibles)
+
+console.log("nonedibles:")
+console.log(nonedibles)
+
+console.log("machinery:")
+console.log(machinery)
+
+document.write("edibles:")
+document.write(edibles)
+document.write("nonedibles:")
+document.write(nonedibles)
+document.write("machinery:")
+document.write(machinery)
+
